f52c88b21df96157ad2ec133ced9974c7c580bef
Mohad009 <26S2053@utas.edu.om>
2025-07-24 10:01:12 +0400

    solution for diff21 problem. I also added .gitignore file to ignore .class files

---
+++ warmup1/Diff21.java
@@ -0,0 +1,13 @@
+package warmup1;
+
+public class Diff21 {
+public static int diff21(int n) {
+  int sub=0;
+  if(n>21)sub=2*(Math.abs(n-21));
+  else sub=21-n;
+  return sub;
+}
+public static void main(String []args){
+    System.out.println(Diff21.diff21(34));
+}
+}
