c41d0df08d22ef865354b6fdee67314ad0e351e7
Mohammed <26S2053@utas.edu.om>
2025-07-27 00:22:30 +0400

    solution for warmup1-in1020 problem

---
+++ warmup1/In1020.java
@@ -0,0 +1,9 @@
+public class In1020 {
+    public static boolean in1020(int a, int b) {
+  if((a>=10 && a<=20) || (b>=10 && b<=20)) return true;
+  return false;
+}
+public static void main(String[] args) {
+    System.out.println(In1020.in1020(5, 15));
+}
+}
