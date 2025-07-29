0e733dd60ebe61e95566cb62a91a16930de3f85b
Mohammed <26S2053@utas.edu.om>
2025-07-29 18:15:24 +0400

    Add solution for problem array1-reverse3

---
+++ array1/Reverse3.java
@@ -0,0 +1,8 @@
+public class Reverse3 {
+    public static int[] reverse3(int[] nums) {
+  return new int[]{nums[2],nums[1],nums[0]};
+}
+public static void main(String[] args) {
+    System.out.println(Reverse3.reverse3(new int[]{3,4,6}));
+}
+}
