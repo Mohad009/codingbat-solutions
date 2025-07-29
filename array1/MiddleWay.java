74301aa4d1b185750a6d24df5068112325da29ab
Mohammed <26S2053@utas.edu.om>
2025-07-29 18:19:30 +0400

    Add solution for problem array1-middleWay

---
+++ array1/MiddleWay.java
@@ -0,0 +1,8 @@
+public class MiddleWay {
+    public static int[] middleWay(int[] a, int[] b) {
+  return new int[]{a[1],b[1]};
+}
+public static void main(String[] args) {
+    System.out.println(MiddleWay.middleWay(new int[]{5,2,9}, new int[]{1,3,6}));
+}
+}
