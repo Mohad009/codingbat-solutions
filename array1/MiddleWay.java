347ea1b61ded01edd705eb812b2724481d00fdac
Mohammed <26S2053@utas.edu.om>
2025-07-29 18:50:38 +0400

    I added Arrays.toString to some problems . this is to print the arrays.

--- array1/MiddleWay.java
+++ array1/MiddleWay.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class MiddleWay {
     public static int[] middleWay(int[] a, int[] b) {
   return new int[]{a[1],b[1]};
 }
 public static void main(String[] args) {
-    System.out.println(MiddleWay.middleWay(new int[]{5,2,9}, new int[]{1,3,6}));
+    System.out.println(Arrays.toString(MiddleWay.middleWay(new int[]{5,2,9}, new int[]{1,3,6})));
 }
 }
