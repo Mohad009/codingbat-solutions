3d2239a518af969ba00224e9099fe76e8591ac89
Mohammed <26S2053@utas.edu.om>
2025-07-30 19:18:32 +0400

    Added solution for the problem logic1-lastDigit

---
+++ logic1/LastDigit.java
@@ -0,0 +1,11 @@
+public class LastDigit {
+    public static boolean lastDigit(int a, int b, int c) {
+  int r1=a%10;
+  int r2=b%10;
+  int r3=c%10;
+  return (r1 == r2) || (r1 == r3) || (r2 == r3);
+}
+public static void main(String[] args) {
+    System.out.println(LastDigit.lastDigit(23, 19, 12));
+}
+}
