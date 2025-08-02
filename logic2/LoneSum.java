cc611479787f9c75861db571511df91bd8e95d29
Mohammed <26S2053@utas.edu.om>
2025-08-02 23:14:05 +0400

    Add solution for problem logic2-loneSum

---
+++ logic2/LoneSum.java
@@ -0,0 +1,12 @@
+public class LoneSum {
+    public static int loneSum(int a, int b, int c) {
+  if(a==b && b==c && a==c) return 0;
+  else if(a==b ) return c;
+  else if(a==c) return b;
+  else if(b==c) return a;
+  else return a+b+c;
+}
+public static void main(String[] args) {
+    System.out.println(LoneSum.loneSum(3,3,3));
+}
+}
