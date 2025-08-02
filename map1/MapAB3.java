cbf6e19665afc125b76dea9ca880fb0dff9c6720
Mohammed <26S2053@utas.edu.om>
2025-08-02 23:08:12 +0400

    Add a solution for problem map1-mapAB3

---
+++ map1/MapAB3.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAB3 {
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+  if(map.containsKey("a") && !map.containsKey("b")) 
+    map.put("b",map.get("a"));
+  else if (map.containsKey("b") && !map.containsKey("a"))
+    map.put("a",map.get("b"));
+  return map;
+}
+public static void main(String[] args) {
+         Map<String, String> myMap = new HashMap<>();
+        myMap.put("a", "aaa");
+        myMap.put("c", "cake");
+    System.out.println(MapAB3.mapAB3(myMap));
+}
+}
